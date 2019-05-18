c9d43fd478fad6abed0eb5201c57ffb1cf6aaf74
Mohamed <mohamedboufraine@MacBook-Pro-de-Mohamed-2.local>
2019-05-18 17:04:32 +0200

    rajout de la methode ajouter une element

--- manipTableau.js
+++ manipTableau.js
@@ -1,5 +1,8 @@
 const tvShow = ["Got", "Narcos", "SNK"];
 
+// Fonctions 18 mai
+// Classe 25 mai
+
 class myArray {
     //Ajouter un element à la fin du tab
     //Ajouter en element en debut de tab
@@ -9,4 +12,17 @@ class myArray {
     //Connaitre la longueur d´un tab
     //Afficher un tableau
 }
+
+//Ajouter en element en debut de tab
+function addElementAtStartArray(tableau, nouvelElement){
+ tableau.push(nouvelElement)
+}
+
+function displayArray(tableau){
+    for(let i = 0; i < tableau.length; i++){
+        console.log()
+    }
+}
+
+addElementAtStartArray(tvShow, "One Piece");
 
