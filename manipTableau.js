961e1879d7a46247bfdb69e3771585ca1b87277c
Mohamed <mohamedboufraine@MacBook-Pro-de-Mohamed-2.local>
2019-05-18 16:56:06 +0200

    Completer la classe

---
+++ manipTableau.js
@@ -0,0 +1,12 @@
+const tvShow = ["Got", "Narcos", "SNK"];
+
+class myArray {
+    //Ajouter un element à la fin du tab
+    //Ajouter en element en debut de tab
+    //Supprimer un element à la fin du tab
+    //Supprimer en element en debut de tab
+    //Ajouter à un endroit spécifique du tab
+    //Connaitre la longueur d´un tab
+    //Afficher un tableau
+}
+
